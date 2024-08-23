a9827ea0cacd2bc3987b0da169e425055124c7ca
yunyouyang <yunyouyang@tencent.com>
2024-08-23 17:50:52 +0800

    chore: upgrade deps

--- src/markdown-it.ts
+++ src/markdown-it.ts
@@ -1,5 +1,5 @@
 import type MarkdownIt from 'markdown-it'
-import type Token from 'markdown-it/lib/token'
+import type Token from 'markdown-it/lib/token.mjs'
 import container from 'markdown-it-container'
 
 export interface CSSI18nOptions {
